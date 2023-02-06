aefea8aec5145dcd73db7a98a4455eaacebab287
tauras <taurastaurenas@gmail.com>
2023-02-06 16:00:14 +0200

    seperate data object

---
+++ src/components/Data.js
@@ -0,0 +1,35 @@
+const data = [
+  {
+    id: 1,
+    name: "pizza",
+    tags: ["meat", "cheese", "dough"],
+    country: "Italy",
+    preparationInstructions: ["1", "2", "3", "4", "5"],
+    ingredientsList: ["butter", "sugar"],
+    imageUrl:
+      "https://nidosreceptai.lt/wp-content/uploads/2018/08/Bulviu-kukuliai-su-varske-6Square-100x100.jpg",
+    videoUrl: "https://www.youtube.com/watch?v=g5oCDoyxbBk",
+  },
+  {
+    id: 2,
+    name: "orange juice",
+    tags: ["orange"],
+    country: "china",
+    preparationInstructions: ["1", "2", "3", "4", "5"],
+    ingredientsList: ["butter", "sugar"],
+    imageUrl: "",
+    videoUrl: "",
+  },
+  {
+    id: 3,
+    name: "cheesburghaa",
+    tags: ["meat", "salad", "buns", "tomato"],
+    country: "USA",
+    preparationInstructions: ["1", "2", "3", "4", "5"],
+    ingredientsList: ["butter", "sugar"],
+    imageUrl: "",
+    videoUrl: "",
+  },
+];
+
+export default data;
--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -2,40 +2,7 @@ import React, { useState } from "react";
 import Edit from "./Edit";
 import NewRecipe from "./NewRecipe";
 import RandomRecipe from "./RandomRecipe";
-
-const data = [
-  {
-    id: 1,
-    name: "pizza",
-    tags: ["meat", "cheese", "dough"],
-    country: "Italy",
-    preparationInstructions: ["1", "2", "3", "4", "5"],
-    ingredientsList: ["butter", "sugar"],
-    imageUrl:
-      "https://nidosreceptai.lt/wp-content/uploads/2018/08/Bulviu-kukuliai-su-varske-6Square-100x100.jpg",
-    videoUrl: "https://www.youtube.com/watch?v=g5oCDoyxbBk",
-  },
-  {
-    id: 2,
-    name: "orange juice",
-    tags: ["orange"],
-    country: "china",
-    preparationInstructions: ["1", "2", "3", "4", "5"],
-    ingredientsList: ["butter", "sugar"],
-    imageUrl: "",
-    videoUrl: "",
-  },
-  {
-    id: 3,
-    name: "cheesburghaa",
-    tags: ["meat", "salad", "buns", "tomato"],
-    country: "USA",
-    preparationInstructions: ["1", "2", "3", "4", "5"],
-    ingredientsList: ["butter", "sugar"],
-    imageUrl: "",
-    videoUrl: "",
-  },
-];
+import data from "./Data";
 
 export default function RecipesList() {
   const [list, setList] = useState(data);
